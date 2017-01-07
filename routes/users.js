1a3cd17249cc8bdc0a63f2f2122a0f24bd846fc8
Nisarga Patel <np8@umbc.edu>
2017-01-07 03:06:40 -0500

    Removed my console logs

--- routes/users.js
+++ routes/users.js
@@ -115,8 +115,6 @@ passport.use(new LocalStrategy(
     passwordField: 'password'
   },
   function(username, password, done) {
-    console.log(username)
-    console.log(password)
 
     User.getUserByUsername(username, function(err, user) {
       if (err) throw err;
